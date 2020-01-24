5849c10c49dae41b16dd7ee7e3184b28f342421e
Facundo Gauna <facundo.gauna@newsignature.com>
2020-01-24 10:04:21 -0500

    wip. able to parse app settings

---
+++ Convert-XmlConfigToSiteConfig.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\$sut"
+
+Describe "Convert-XmlConfigToSiteConfig" {
+    It "Converts to app service app settings" {
+        $doc = [xml]@'
+<?xml version="1.0" encoding="utf-8"?>
+<configuration>
+    <appSettings>
+        <!--Website-->
+        <add key="SitePath" value="http://google.com" />
+        <add key="WebSiteBase" value="dev" />
+        <add key="dev_or_prod" value="dev" />
+        <add key="Config" value="Dev" />
+    </appSettings>
+</configuration>
+'@
+
+        $doc | Convert-XmlConfigToSiteConfig
+        
+    }
+}
---
+++ Convert-XmlConfigToSiteConfig.ps1
@@ -0,0 +1,23 @@
+﻿function Convert-XmlConfigToSiteConfig {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline = $true)]
+        [xml]
+        $XmlConfig
+    )
+
+    foreach ($appSetting in $XmlConfig.configuration.appSettings.add){
+#         $segment = @"
+# {
+#     "name": "SitePath",
+#     "value": "[parameters('configSitePath')]"
+# },
+# "@      
+        Write-Host "app setting key $($appSetting.key)"
+        Write-Host "app setting key $($appSetting.value)"
+    }
+
+    
+
+
+}
